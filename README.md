dc89e96fe748f85a569d61b14cf106cc7f9c0feb
koushik <koushik.dey@live.com>
2022-08-15 10:54:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<div align="center"><a href="https://www.youtube.com/watch?v=Ry_r8DCj3hw">CORS PRACTICE with Sumit</a></div><br>
---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"],
+    })
+);
+
+app.put("/data", (req, res) => {
+    res.json({
+        name: "Finland",
+        description: "Land of Sauna & Lakes",
+    });
+});
